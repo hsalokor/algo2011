d0ea375583f96bd18011d5a1cb78266912a67d8a
Jakub Järvenpää <jakub.jarvenpaa@reaktor.fi>
2011-03-04 00:01:17 +0200

    Cleaned up test fixture.

--- test/KnapsackTest.hs
+++ test/KnapsackTest.hs
@@ -2,5 +2,7 @@ module KnapsackTest where
 import Test.HUnit
 import Knapsack
 
-totalWeightTest = TestCase (assertEqual "Total weight" [12, 15, 18] (totalWeight [[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-tests = TestList [TestLabel "test" totalWeightTest]
+totalWeightTest = TestCase (assertEqual "Total weight is calculated correctly" [12, 15, 18] (totalWeight [[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+tests = TestList [ TestLabel "" totalWeightTest
+                 ]
