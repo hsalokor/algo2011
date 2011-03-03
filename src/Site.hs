df154175b84f8b9ffd739758f8cab1ef947cd8cb
Harri Salokorpi <hsalokor@iki.fi>
2011-03-03 22:46:15 +0200

    Fix request handling

--- src/Site.hs
+++ src/Site.hs
@@ -12,27 +12,29 @@ import           Snap.Types
 import           Text.JSON
 import           Text.JSON.Generic
 
-import           Data.ByteString.Char8
+import           Data.ByteString.Lazy.Char8
 
 import           Application
 import           Knapsack
 import           First
 
-decodeData body = decodeJSON body :: KnapsackProblem
-
-process :: KnapsackProblem -> [String]
-process d = solve d
 
 solver :: Application ()
 solver = ifTop $ do
         body <- getRequestBody
         modifyResponse $ setResponseStatus 200 ""
         modifyResponse $ addHeader "Content-Type" "application/json"
-        writeBS $ handle $ body
+        writeLBS $ handle $ body
         r <- getResponse
         finishWith r
     where
-        handle body = pack $ encodeJSON $ process $ decodeData $ show body
+        handle body = formatOutput $ solve $ readInput body
+        formatOutput result = pack $ encodeJSON result
+        process input = case input of 
+                            Ok problem -> solve problem
+                            Error e   -> [e]
+        readInput body = decodeJSON input :: KnapsackProblem
+                         where input = unpack body
 
 site :: Application ()
 site = route [ ("/", solver) ]
