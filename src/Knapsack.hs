6616f40d09841419445acfa3c860183ce01cda8b
Harri Salokorpi <hsalokor@iki.fi>
2011-03-03 17:37:54 +0200

    Trim down dependencies

---
+++ src/Knapsack.hs
@@ -0,0 +1,22 @@
+module Knapsack
+  ( Item
+  , KnapsackProblem
+  , Dimensions
+  ) where
+
+data Dimensions = Dimensions Int Int Int
+    derives(Show)
+
+data KnapsackProblem = KnapsackProblem {
+                         name :: String,
+                         timeout :: Int,
+                         items :: [Item],
+                         knapsackDimensions :: Dimensions
+    }
+    derives(Show)
+
+data Item = Item { id :: Int
+                 , value :: Int,
+                 , itemDimensions :: Dimensions
+                 }
+    derives(Show)
