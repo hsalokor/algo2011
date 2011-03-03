4550a6e6626aae7750ccb1176f219b79435c1577
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-04 01:38:19 +0200

    fix fits function

--- test/KnapsackTest.hs
+++ test/KnapsackTest.hs
@@ -7,7 +7,7 @@ totalWeightTest =
 remainingCapacityTest = 
     TestCase (assertEqual "Remaining capacity" [88, 85, 82] (remainingCapacity [100, 100, 100] [[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
 fitTestFits = 
-    TestCase (assertEqual "Fits" True (fit [100, 100, 100] [[1, 2, 3]]))
+    TestCase (assertEqual "Fits" True (fit [100, 100, 100] [[100, 2, 3]]))
 fitTestDoesNotFit = 
     TestCase (assertEqual "Does not fit" False (fit [1, 2, 2] [[1, 2, 3]]))    
 
