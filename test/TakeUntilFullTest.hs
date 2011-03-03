4b9303db5c886a402a09e8736bd19bbb5cfa7a60
Jakub Järvenpää <jakub.jarvenpaa@reaktor.fi>
2011-03-04 00:39:27 +0200

    Fixture added.

--- test/TakeUntilFullTest.hs
+++ test/TakeUntilFullTest.hs
@@ -5,6 +5,8 @@ import Knapsack
 lolTest = 
     TestCase (assertEqual 1 1)
 
+-- KnapsackProblem "sample round" 6000 [Item "1" 40 [10, 20, 30], Item "2" 50 [20, 30, 40], Item "3" 60 [30, 40, 50]] [50, 100, 300]
+
 takeUntilFullTests = TestList [
                          , TestLabel "" lolTest
                          ]
