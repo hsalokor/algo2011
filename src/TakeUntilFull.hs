78f9d27e431f47ac212df011133602d7574ac551
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-04 01:13:42 +0200

    refactor

--- src/TakeUntilFull.hs
+++ src/TakeUntilFull.hs
@@ -5,7 +5,10 @@ import Data.List
 solve :: KnapsackProblem -> [String]
 solve problem = takeUntilFull [] (contents problem) (capacity problem)
 
-takeUntilFull solution [] capacity = map Knapsack.id solution
+takeUntilFull solution [] capacity = ids solution
 takeUntilFull solution contents capacity = 
-    if fit capacity (map weight ((head contents) : solution)) then takeUntilFull ((head contents) : solution) (tail contents) capacity
-    else map Knapsack.id solution  
+    if fit capacity (map weight oneMore) then takeUntilFull oneMore (tail contents) capacity
+    else ids solution  
+    where oneMore = ((head contents) : solution)
+    
+ids solution = map Knapsack.id solution
