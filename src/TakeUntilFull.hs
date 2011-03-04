f7526568ba80a61e1b4cee391308c5f6772f3bdd
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-04 11:18:04 +0200

    reimplement TakeUntilFull

--- src/TakeUntilFull.hs
+++ src/TakeUntilFull.hs
@@ -1,14 +1,23 @@
 module TakeUntilFull where
 import Knapsack
 import Data.List
+import First
 
-solve :: Knapsack -> [String]
-solve problem = takeUntilFull [] (available problem) (capacity problem)
+solve :: Knapsack -> Knapsack
 
-takeUntilFull solution [] capacity = ids solution
+solve = takeUntilFull
+
+takeUntilFull :: Knapsack -> Knapsack
+takeUntilFull problem@(Knapsack _ [] _ _) = problem
+takeUntilFull problem | fit (capacity problem) (map weight $ selected oneMore) = oneMore
+                      | otherwise = problem
+    where oneMore = takeUntilFull $ First.solve problem
+
+{-|
 takeUntilFull solution contents capacity = 
     if fit capacity (map weight oneMore) then takeUntilFull oneMore (tail contents) capacity
     else ids solution  
     where oneMore = ((head contents) : solution)
     
 ids solution = map Knapsack.id solution
+-}
--- test/TakeUntilFullTest.hs
+++ test/TakeUntilFullTest.hs
@@ -3,15 +3,19 @@ import Test.HUnit
 import Knapsack
 import TakeUntilFull
 
-items = [Item "1" 20 [1, 1, 1], Item "2" 10 [1, 1, 1], Item "3" 30 [1, 1, 1]]
+item1 = Item "1" 20 [1, 1, 1]
+item2 = Item "2" 10 [1, 1, 1]
+item3 = Item "3" 30 [1, 1, 1]
+
+items = [item1, item2, item3]
 problem capacity items = Knapsack 6000 items [] capacity
 
 noneFit = 
-    TestCase (assertEqual "None fit" [] (solve (problem [0, 1000, 1000] items)))
+    TestCase (assertEqual "None fit" [] (selected $ solve (problem [0, 1000, 1000] items)))
 someFit =
-    TestCase (assertEqual "Two first items fit" ["2", "1"] (solve (problem [2, 1000, 1000] items)))
+    TestCase (assertEqual "Two first items fit" [item2, item1] (selected $ solve (problem [2, 1000, 1000] items)))
 allFit =
-    TestCase (assertEqual "All fit" ["3", "2", "1"] (solve (problem [1000, 1000, 1000] items)))
+    TestCase (assertEqual "All fit" [item3, item2, item1] (selected $ solve (problem [1000, 1000, 1000] items)))
 
 takeUntilFullTests = [ TestLabel "" noneFit
                      , TestLabel "" someFit
