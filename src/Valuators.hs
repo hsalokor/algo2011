5a59256d1a249e2da2b54c9e927bd28a9da4668d
Jakub Järvenpää <jakub.jarvenpaa@reaktor.fi>
2011-03-04 18:24:59 +0200

    Cleaned up compiler warnings.

--- src/Valuators.hs
+++ src/Valuators.hs
@@ -6,7 +6,7 @@ valuePerWeight item = toRational(value item) / toRational(sum $ weight item)
 
 valuePerQuadraticWeight :: Item -> Rational
 valuePerQuadraticWeight item = toRational(value item) / toRational(sum $ map pow2 (weight item))
-    where pow2 value = value ^ 2
+    where pow2 value = (value :: Int) ^ (2 :: Int)
     
 powWeight :: Item -> Rational
 powWeight item = toRational((value item) ^ n) / toRational(sum $ map powN (weight item))
