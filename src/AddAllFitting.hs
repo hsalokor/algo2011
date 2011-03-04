29fc3929aaf04a2f9f8018604def26ea2cc7aa34
Jakub Järvenpää <jakub.jarvenpaa@reaktor.fi>
2011-03-04 23:33:20 +0200

    Refactor.

--- src/AddAllFitting.hs
+++ src/AddAllFitting.hs
@@ -6,7 +6,7 @@ solve :: Knapsack -> Knapsack
 solve problem = addAllFitting (length $ available problem) problem
 
 addAllFitting :: Int -> Knapsack -> Knapsack
-addAllFitting left problem@(Knapsack _ [] _ _) = problem
+addAllFitting _ problem@(Knapsack _ [] _ _) = problem
 addAllFitting 0 problem = problem
 addAllFitting left problem | isValid proposed = addAllFitting (left) proposed
                            | otherwise = addAllFitting (left - 1) (rotate problem)
