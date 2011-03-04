0a42c1ce393f13527121e9f55a855abf44532796
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-04 15:01:11 +0200

    add valuePerQuadraticWeight valuator

--- src/Valuators.hs
+++ src/Valuators.hs
@@ -5,3 +5,7 @@ import GHC.Exts
 
 valuePerWeight :: Item -> Rational
 valuePerWeight item = toRational(value item) / toRational(sum $ weight item)
+
+valuePerQuadraticWeight :: Item -> Rational
+valuePerQuadraticWeight item = toRational(value item) / toRational(sum $ map pow2 (weight item))
+    where pow2 value = value ^ 2
--- test/ValuatorsTest.hs
+++ test/ValuatorsTest.hs
@@ -5,6 +5,9 @@ import Valuators
 
 valuePerWeightTest = 
     TestCase (assertEqual "Value per weight" 0.1 (valuePerWeight (Item "id" 10 [50, 30, 20])))        
+valuePerQuadraticWeightTest = 
+    TestCase (assertEqual "Value per weight" 0.1 (valuePerQuadraticWeight (Item "id" 30 [10, 10, 10])))        
 
 valuatorTests = [ TestLabel "" valuePerWeightTest
-                     ]
+                , TestLabel "" valuePerQuadraticWeightTest
+                ]
