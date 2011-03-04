4e75739515b1b089da345e8c7999550b4b8215e0
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-04 15:31:57 +0200

    Parallelize

--- src/PickMostValuable.hs
+++ src/PickMostValuable.hs
@@ -3,8 +3,14 @@ module PickMostValuable where
 import Knapsack
 import Data.List
 import GHC.Exts
+import Control.Parallel (par)
 
 solve :: [Knapsack -> Knapsack] -> Knapsack -> Knapsack
 
-solve algs problem = head $ reverse $ sortWith knapsackValue $ map call algs
+solve algs problem = head $ reverse $ sortWith knapsackValue $ parMap call algs
     where call f = f problem
+    
+parMap :: (a -> b) -> [a] -> [b]
+parMap f (x:xs) = r `par` (r : parMap f xs)
+                  where r = f x
+parMap _ _      = []
