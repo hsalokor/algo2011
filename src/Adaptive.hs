860e18cf8e53446b65214deaeefde9a3d3749048
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-05 16:47:55 +0200

    Optimize Adaptive by sorting only once per 10 items

--- src/Adaptive.hs
+++ src/Adaptive.hs
@@ -3,7 +3,10 @@ import Knapsack
 import First
 
 solve :: (Knapsack -> Knapsack) -> Knapsack -> Knapsack
-solve sort problem | (available $ withoutOversize problem) == [] = problem
-                   | otherwise = (Adaptive.solve sort) $ First.solve $ sort $ withoutOversize problem
+solve sort problem = adaptive sort 10 problem
+adaptive :: (Knapsack -> Knapsack) -> Int  -> Knapsack -> Knapsack
+adaptive sort count problem | (available $ withoutOversize problem) == [] = problem
+                            | (count > 0) = (adaptive sort (count - 1)) $ First.solve $ withoutOversize problem
+                            | otherwise = (adaptive sort 10) $ First.solve $ sort $ withoutOversize problem
                         
 withoutOversize k = k { available = dropTooBig (available k) (remainingSpace k)}  
